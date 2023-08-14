27921b7c69ddb6d162a4b2f56fc6d747a1cd2465
William Soares da Silva <will_silva_2011@hotmail.com>
2023-08-14 16:02:21 -0300

    teste startDB

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,9 +1,116 @@
 class CaixaDaLanchonete {
-
-    calcularValorDaCompra(metodoDePagamento, itens) {
-        return "";
+    constructor() {
+        this.cardapio = {
+            cafe: 3.00,
+            chantily: 1.50,
+            suco: 6.20,
+            sanduiche: 6.50,
+            queijo: 2.00,
+            salgado: 7.25,
+            combo1: 9.50,
+            combo2: 7.50
+        };
+        this.formasDePagamento = [ 'dinheiro', 'debito', 'credito' ];
+       
+        this.descontos = {
+            dinheiro: 0.95,
+            debito: 1.00,
+            credito: 1.03
+        };
+        
+        this.itensExtras = {
+            chantily: 'cafe',
+            queijo: 'sanduiche'
+        };
     }
-
+    
+    calcularValorDaCompra( formaDePagamento, itens ) {
+        if ( itens.length === 0 ) {
+            return "Não há itens no carrinho de compra!";
+        }
+        
+        if ( !this.formasDePagamento.includes ( formaDePagamento )) {
+            return "Forma de pagamento inválida!";
+        }
+        
+        let total = 0;
+        for ( let item of itens ) {
+            let [ codigo, quantidade ] = item.split(',');
+            quantidade = parseInt( quantidade );
+            
+            if ( quantidade === 0 ) {
+                return "Quantidade inválida!";
+            }
+            
+            if ( !this.cardapio.hasOwnProperty( codigo )) {
+                return "Item inválido!";
+            }
+            
+            if ( this.itensExtras.hasOwnProperty( codigo ) && !itens.some(i => i.startsWith(this.itensExtras[codigo]))) {
+                return "Item extra não pode ser pedido sem o principal";
+            }
+            
+            total += this.cardapio[ codigo ] * quantidade;
+        }
+        
+        total *= this.descontos[formaDePagamento];
+        
+        return `R$ ${total.toFixed(2).replace('.', ',')}`;
+    }
 }
 
+class Sabores{
+    constructor(){
+
+        this.itensDoces = [ 'cafe', 'chantily', 'suco' ];
+        this.itensSalgados = [ 'sanduiche', 'queijo', 'salgado', ];
+        
+    }
+
+    verificadorDeSabor(item){
+
+        if(this.itensDoces.includes(item)){
+
+            return 'doce';
+
+        } else if (this.itensSalgados.includes(item)){
+            
+            return 'salgado';
+
+        }else{
+
+            return 'desconhecido';
+        }
+    }
+}
+
+class Variados {
+    constructor() {
+
+        this.itensComDoce = ['combo1', 'combo2'];
+
+    }
+
+    contemDoce(item) {
+
+        return this.itensComDoce.includes(item);
+
+    }
+}
+
+
+const caixa1 = new CaixaDaLanchonete();
+const resultado1 = caixa1.calcularValorDaCompra( 'debito', [ 'chantily,1' ]);
+console.log( resultado1 ); 
+
+const caixa2 = new CaixaDaLanchonete();
+const resultado2 = caixa2.calcularValorDaCompra( 'credito', [ 'cafe,1','chantily,1' ]);
+console.log( resultado2 );
+
+const caixa3 = new CaixaDaLanchonete();
+const resultado3 = caixa3.calcularValorDaCompra( 'credito', [ "combo1, 1", 'cafe,2' ]);
+console.log( resultado3 );
+
 export { CaixaDaLanchonete };
+export { Sabores };
+export { Variados };
--- src/caixa-da-lanchonete.test.js
+++ src/caixa-da-lanchonete.test.js
@@ -1,4 +1,4 @@
-import { CaixaDaLanchonete } from "./caixa-da-lanchonete.js";
+import { CaixaDaLanchonete, Variados, Sabores } from "./caixa-da-lanchonete.js";
 
 describe('CaixaDaLanchonete', () => {
 
@@ -44,8 +44,65 @@ describe('CaixaDaLanchonete', () => {
     test.each([
         ['chantily', 'dinheiro', 'Item extra não pode ser pedido sem o principal', ['chantily,1']],
         ['queijo', 'credito', 'Item extra não pode ser pedido sem o principal', ['queijo,1']],
-        ['chantily com outro item', 'credito', 'Item extra não pode ser pedido sem o principal', ['chantily,1', 'sanduiche,1']],
-        ['queijo com outro item', 'debito', 'Item extra não pode ser pedido sem o principal', ['cafe,1', 'queijo,1']],
+        ['chantily com sanduiche', 'credito', 'Item extra não pode ser pedido sem o principal', ['chantily,1', 'sanduiche,1']],
+        ['queijo com café', 'debito', 'Item extra não pode ser pedido sem o principal', ['cafe,1', 'queijo,1']],
     ])('compra %p em %p deve resultar em %p', (_, formaDePagamento, resultadoEsperado, itens) =>
         validaTeste(formaDePagamento, resultadoEsperado, itens));
+    
+    /*
+    test.each([
+        ['dinheiro', 'R$ 15,70', ['cafe,1', 'sanduiche,1', 'suco,1']],
+        ['credito', 'R$ 9,27', ['cafe,1', 'sanduiche,1', 'suco,1']],
+        ['debito', 'R$ 9,00', ['cafe,1', 'sanduiche,1', 'suco,1']],
+    ])('compra de 3 itens em %p deve resultar em %p', validaTeste);
+        
+    test.each([
+        ['dinheiro', 'R$ 11,40', ['cafe,2', 'sanduiche,2']],
+        ['credito', 'R$ 12,36', ['cafe,2', 'sanduiche,2']],
+        ['debito', 'R$ 12,00', ['cafe,2', 'sanduiche,2']],
+    ])('compra de múltiplas quantidades em %p deve resultar em %p', validaTeste);
+        
+     test.each([
+        ['dinheiro', 'R$ 17,10', ['cafe,2', 'sanduiche,2', 'suco,2']],
+        ['credito', 'R$ 18,54', ['cafe,2', 'sanduiche,2', 'suco,2']],
+         ['debito', 'R$ 18,00', ['cafe,2', 'sanduiche,2', 'suco,2']],
+    ])('compra de múltiplas quantidades de múltiplos itens em %p deve resultar em %p',
+        validaTeste);
+    */
+
+    test('Essa forma de pagamento não está disponível', () => {
+        const caixa = new CaixaDaLanchonete();
+        const resultado = caixa.calcularValorDaCompra('Ouro', ['cafe,1']);
+        expect(resultado).toEqual('Forma de pagamento inválida!');
+        });
 });
+
+describe('Sabores', () => {
+    test.each([
+      ['cafe', 'doce'],
+      ['chantily', 'doce'],
+      ['suco', 'doce'],
+      ['sanduiche', 'salgado'],
+      ['queijo', 'salgado'],
+      ['salgado', 'salgado'],
+      ['item desconhecido', 'desconhecido']
+    ])('verificarSabor deve retornar %p para o item %p', (item, saborEsperado) => {
+      const verificador = new Sabores();
+      const sabor = verificador.verificadorDeSabor(item);
+      expect(sabor).toEqual(saborEsperado);
+    });
+  });
+
+describe('Variados', () => {
+    test.each([
+      ['combo1', true],
+      ['combo2', true],
+      ['cafe', false],
+      ['sanduiche', false],
+      ['item desconhecido', false]
+    ])('contemDoce deve retornar %p para o item %p', (item, resultadoEsperado) => {
+      const verificador = new Variados();
+      const resultado = verificador.contemDoce(item);
+      expect(resultado).toEqual(resultadoEsperado);
+    });
+  });
